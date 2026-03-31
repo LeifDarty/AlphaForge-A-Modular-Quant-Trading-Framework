d87f66118c4d1fea6d2fcc0575a9d39a0fd1b1fd
YOGESH CHOUDHARY <72182147+LeifDarty@users.noreply.github.com>
2026-03-31 08:04:25 +0530

    Add files via upload

---
+++ signal_engine.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class SignalEngine:
+
+    def __init__(self, data):
+        self.data = data
+
+    def generate_signals(self):
+
+        self.data['signal'] = np.select(
+            [
+                (self.data['close'].shift(1) > self.data['lower']) &
+                (self.data['low'] < self.data['lower']),
+
+                (self.data['close'].shift(1) < self.data['upper']) &
+                (self.data['high'] > self.data['upper'])
+            ],
+            [1, -1],
+            default=0
+        )
+
+        return self.data
